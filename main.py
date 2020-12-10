11c7370860cbc73fabd1eaa1c4ad4f7bbcbb9b1c
ViktorZhikharev <viktorjiharev@rambler.ru>
2020-12-10 17:14:58 +0500

    v 0.2

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('flags.ui', self)
+        self.rb1.clicked.connect(self.col1)
+
+    def col1(self):
+        pass
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
