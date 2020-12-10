69c699dc21c5d7ce79327e3c60a027937221f1aa
ViktorZhikharev <viktorjiharev@rambler.ru>
2020-12-10 17:48:01 +0500

    v 1.0

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-
+import sys, random
 from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor, QFont
 from PyQt5.QtWidgets import *
 
 
@@ -9,9 +9,28 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('UI.ui', self)
         self.rb1.clicked.connect(self.col1)
+        self.cirk = []
 
     def col1(self):
-        pass
+        a = random.randint(0, 100)
+        b = random.randint(0, 240)
+        c = random.randint(0, 250)
+        self.cirk.append((b, c, a, a))
+        self.repaint()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw(qp)
+        qp.end()
+
+    def draw(self, qp):
+        col = QColor()
+        col.setNamedColor('yellow')
+        for i in range(len(self.cirk)):
+            qp.setBrush(col)
+            qp.drawEllipse(self.cirk[i][0], self.cirk[i][1], self.cirk[i][2], self.cirk[i][2])
+            print(self.cirk[i][0], self.cirk[i][1], self.cirk[i][2], self.cirk[i][2])
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
